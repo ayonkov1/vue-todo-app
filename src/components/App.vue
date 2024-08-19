15599ed0f7c57c902e7c57ab5adc89d231d9b089
Atanas Yonkov <atanasyonkov1@gmail.com>
2024-08-19 21:53:17 +0300

    Implement delete todo button

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,29 +1,50 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 
-const input = ref(1)
+const todos = reactive([
+  {
+    title: 'Buy groceries',
+    completed: false
+  },
+  {
+    title: 'Finish project report',
+    completed: false
+  }
+])
 const textInput = ref('')
 
-const increment = () => {
-  input.value++
-}
-
-const onChangeHandler = (event: any) => {
+const onClickHandler = (event: any) => {
   if (event) {
     event.preventDefault()
   }
+
+  todos.push({
+    title: textInput.value,
+    completed: false
+  })
+}
+
+const onChangeHandler = (event: any) => {
   textInput.value = event.target.value
+}
+
+const removeTodo = (title: string) => {
+  // todos = todos.filter((todo) => todo.title !== title)     -- in case we go for ref
+  var index = todos.findIndex((todo) => todo.title === title)
+  todos.splice(index, 1)
 }
 </script>
 
 <template>
   <header>
-    <div class="wrapper">Counter: {{ input }}</div>
-    <button @click="increment">Increase count</button>
+    <div v-for="(todo, index) of todos" :key="index">
+      {{ todo.title }}
+      <span @click="removeTodo(todo.title)" style="cursor: pointer">❌</span>
+    </div>
   </header>
 
   <main>
     <input type="text" @input="onChangeHandler" />
-    <div>{{ textInput }}</div>
+    <button @click="onClickHandler">Add ToDo</button>
   </main>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './components/App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
