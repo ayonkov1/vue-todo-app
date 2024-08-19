df95694c89bdda3aa72043008e3c065196f34b51
Atanas Yonkov <atanasyonkov1@gmail.com>
2024-08-19 20:48:10 +0300

    Add simple button increment and input event handler

---
+++ src/components/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const input = ref(1)
+const textInput = ref('')
+
+const increment = () => {
+  input.value++
+}
+
+const onChangeHandler = (event: any) => {
+  if (event) {
+    event.preventDefault()
+  }
+  textInput.value = event.target.value
+}
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">Counter: {{ input }}</div>
+    <button @click="increment">Increase count</button>
+  </header>
+
+  <main>
+    <input type="text" @input="onChangeHandler" />
+    <div>{{ textInput }}</div>
+  </main>
+</template>
